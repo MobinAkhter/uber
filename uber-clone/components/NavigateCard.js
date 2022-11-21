55ffeceef21eaca23664be5d8c02180344692e97
Mobin Akhter <akhtemob@sheridancollege.ca>
2022-11-21 16:43:53 -0500

    Everything good so far

--- uber-clone/components/NavigateCard.js
+++ uber-clone/components/NavigateCard.js
@@ -6,6 +6,7 @@ import { GOOGLE_MAPS_APIKEY } from "@env";
 import { useDispatch } from "react-redux";
 import { setDestination } from "../slices/navSlice";
 import { useNavigation } from "@react-navigation/native";
+import NavFavorites from "./NavFavorites";
 
 const NavigateCard = () => {
   const dispatch = useDispatch();
@@ -13,32 +14,35 @@ const NavigateCard = () => {
   return (
     <SafeAreaView style={tw`bg-white flex-1`}>
       <Text style={tw`text-center py-5 text-xl`}>Good Morning, Mobin</Text>
-      <View style={tw`border-t border-gray-200 flex-shrink`}></View>
-      <View>
-        <GooglePlacesAutocomplete
-          placeholder="Where to?"
-          styles={toInputBoxStyles}
-          fetchDetails={true}
-          returnKeyType={"search"}
-          minLength={2}
-          onPress={(data, details = null) => {
-            dispatch(
-              setDestination({
-                location: details.geometry.location,
-                description: data.description,
-              })
-            );
-            navigation.navigate("RideOptionsCard");
-          }}
-          enablePoweredByContainer={false}
-          query={{
-            key: GOOGLE_MAPS_APIKEY,
-            language: "en",
-          }}
-          debounce={400}
-          nearbyPlacesAPI="GooglePlacesSearch"
-        />
+      <View style={tw`border-t border-gray-200 flex-shrink`}>
+        <View>
+          <GooglePlacesAutocomplete
+            placeholder="Where to?"
+            styles={toInputBoxStyles}
+            fetchDetails={true}
+            returnKeyType={"search"}
+            minLength={2}
+            onPress={(data, details = null) => {
+              dispatch(
+                setDestination({
+                  location: details.geometry.location,
+                  description: data.description,
+                })
+              );
+              navigation.navigate("RideOptionsCard");
+            }}
+            enablePoweredByContainer={false}
+            query={{
+              key: GOOGLE_MAPS_APIKEY,
+              language: "en",
+            }}
+            debounce={400}
+            nearbyPlacesAPI="GooglePlacesSearch"
+          />
+        </View>
+        <NavFavorites />
       </View>
+      <View></View>
     </SafeAreaView>
   );
 };
